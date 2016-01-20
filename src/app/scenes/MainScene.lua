c6f4e385dd68f27c2ce0483ec66801ea039d21cc
Guoxing.song <980538137@qq.com>
2016-01-20 15:40:33 +0800

    no message

--- src/app/scenes/MainScene.lua
+++ src/app/scenes/MainScene.lua
@@ -20,32 +20,34 @@ function MainScene:ctor()
         elseif eventType == ccui.TouchEventType.ended then
             printf("Touch Down")
 
-            function onRequestFinished(event)
-                local ok = (event.name == "completed")
-                local request = event.request
+            -- function onRequestFinished(event)
+            --     local ok = (event.name == "completed")
+            --     local request = event.request
 
-                if not ok then
-                    --请求失败，显示错误代码和错误信息
-                    print(request:getErrorCode(), request:getErrorMessage())
-                    return
-                end
+            --     if not ok then
+            --         --请求失败，显示错误代码和错误信息
+            --         print(request:getErrorCode(), request:getErrorMessage())
+            --         return
+            --     end
 
-                local code = request:getResponseStatusCode()
-                if code ~= 200 then
-                    -- 请求结束，但没有返回 200 响应代码
-                    print("failed"..code)
-                    return
-                end
+            --     local code = request:getResponseStatusCode()
+            --     if code ~= 200 then
+            --         -- 请求结束，但没有返回 200 响应代码
+            --         print("failed"..code)
+            --         return
+            --     end
 
-                --请求成功
-                local response = request:getResponseString()
-                print(response)
-            end
-            --创建一个链接，
-            local url = "http://182.92.237.220:8080/dyncfg?channelId=101&ver=1.3.3"
-            local request = network.createHTTPRequest(onRequestFinished, url, "GET")
-            --开始请求
-            request:start()
+            --     --请求成功
+            --     local response = request:getResponseString()
+            --     print(response)
+            -- end
+            -- --创建一个链接，
+            -- -- local url = "http://182.92.237.220:8080/dyncfg?channelId=101&ver=1.3.3"
+            -- local url = "http://192.168.1.124:8080/dyncfg?channelId=101&ver=1.3.3"
+            -- local request = network.createHTTPRequest(onRequestFinished, url, "GET")
+            -- --开始请求
+            -- request:start()
+            self:checkVersion()
         elseif eventType == ccui.TouchEventType.canceled then
 
         end
@@ -95,6 +97,53 @@ function MainScene:onEnter()
 end
 
 function MainScene:onExit()
+end
+
+function MainScene:checkVersion()
+    --从服务器检测版本号
+    function onRequestFinished(event)
+        local ok = (event.name == "completed")
+        local request = event.request
+
+        if not ok then
+            --请求失败，显示错误代码和错误信息
+            print(request:getErrorCode(), request:getErrorMessage())
+            return
+        end
+
+        local code = request:getResponseStatusCode()
+        if code ~= 200 then
+            -- 请求结束，但没有返回 200 响应代码
+            print("failed"..code)
+            return
+        end
+
+        --请求成功
+        local response = request:getResponseString()
+        print(response)
+
+        local cjson = require("cjson")
+        local status, result = pcall(cjson.decode, response)
+        if status then 
+            print("data:"..result["IsNeedUpdate"])
+            local IsNeedUpdate = result["IsNeedUpdate"]
+            if IsNeedUpdate == 1 then --是否需要更新
+                local IsAllUpdate = result["IsAllUpdate"]
+                if IsAllUpdate  == 1 then  --是否整包更新
+                    --进入下载页面
+                else
+                    --增量更新
+                end
+            else
+                --直接进入游戏
+            end
+        end
+    end
+    -- local url = "http://182.92.237.220:8080/dyncfg?channelId=101&ver=1.3.3"
+    local url = "http://127.0.0.1:8080/check_update?app_version=1&script_version=1.0.0&channel=test"
+    local request = network.createHTTPRequest(onRequestFinished, url, "GET")
+    --开始请求
+    request:start()
 end
 
 return MainScene
